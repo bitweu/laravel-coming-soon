d3b965ef2ff0bca3d8b18f17bb541e8c4c157f02
Nerijus Stakutis <nnerijjuss@gmail.com>
2019-02-24 07:20:52 +0000

    Apply fixes from StyleCI

--- src/ComingSoonServiceProvider.php
+++ src/ComingSoonServiceProvider.php
@@ -2,25 +2,24 @@
 
 namespace Bitweu\ComingSoon;
 
-use Bitweu\ComingSoon\ComingSoon;
 use Illuminate\Support\ServiceProvider;
 
 class ComingSoonServiceProvider extends ServiceProvider
 {
-	/**
-	 * Bootstrap the application services
-	 */
-	public function boot()
-	{
-		// Load package assets
-		$this->loadViewsFrom(__DIR__.'/../resources/views', 'coming-soon');
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        // Load package assets
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'coming-soon');
 
-		// Load helpers
-		$this->loadHelpers();
+        // Load helpers
+        $this->loadHelpers();
 
-		if($this->app->runningInConsole()) {
-			// Publishing the config
-			$this->publishes([
+        if ($this->app->runningInConsole()) {
+            // Publishing the config
+            $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('coming-soon.php'),
             ], 'config');
 
@@ -33,29 +32,29 @@ class ComingSoonServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../public' => public_path('vendor/coming-soon'),
             ], 'assets');
-		}
-	}
+        }
+    }
 
-	/**
-	 * Register the application services
-	 */
-	public function register()
-	{
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
         // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'coming-soon');
 
         $this->app->singleton('coming-soon', function () {
             return new ComingSoon(config('coming-soon.title'), config('coming-soon.intro'), config('coming-soon.background'));
         });
-	}
+    }
 
-	/**
-	 * Register package helpers
-	 */
-	protected function loadHelpers()
-	{
-		if (file_exists($file = __DIR__.'/helpers.php')) { 
-		    require_once $file;
-		}
-	}
+    /**
+     * Register package helpers.
+     */
+    protected function loadHelpers()
+    {
+        if (file_exists($file = __DIR__.'/helpers.php')) {
+            require_once $file;
+        }
+    }
 }
--- tests/ComingSoonTest.php
+++ tests/ComingSoonTest.php
@@ -2,15 +2,15 @@
 
 namespace Bitweu\ComingSoon\Tests;
 
-use Bitweu\ComingSoon\ComingSoon;
 use PHPUnit\Framework\TestCase;
+use Bitweu\ComingSoon\ComingSoon;
 
 class ComingSoonTest extends TestCase
 {
     /** @test */
     public function get_coming_soon_data()
     {
-    	$comingSoon = new ComingSoon('Coming soon title', 'Coming soon intro', 'coming-soon-background.jpg');
+        $comingSoon = new ComingSoon('Coming soon title', 'Coming soon intro', 'coming-soon-background.jpg');
 
         $this->assertTrue($comingSoon->title() == 'Coming soon title');
         $this->assertTrue($comingSoon->intro() == 'Coming soon intro');
